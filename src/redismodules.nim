80df705cc3adbcfa5fa9fe7eb8aa3625abadf8e2
luisacosta <luisacosta828@gmail.com>
2021-06-05 20:48:31 -0400

    added support for long, string and array data types

--- src/redismodules.nim
+++ src/redismodules.nim
@@ -27,6 +27,10 @@ var ReplyWithLongLong* {.redis_extern.}: proc(ctx: ptr Ctx, ll: clonglong):cint 
 
 var ReplyWithSimpleString* {.redis_extern.}: proc(ctx: ptr Ctx, msg: const_char_pp): cint {.cdecl.}
 
+var ReplyWithArray* {.redis_extern.}: proc(ctx: ptr Ctx, len: clong):cint {.cdecl.}
+
+var ReplySetArrayLength* {.redis_extern.}: proc(ctx: ptr Ctx, len: clong) {.cdecl.}
+
 var CreateCommand* {. redis_extern .}: proc(ctx: ptr Ctx,name: const_char_pp, 
                                     cmdfunc: CmdFunc, strflags: const_char_pp,
                                     firstkey, lastkey, keystep: cint):cint {. cdecl .}
@@ -38,23 +42,30 @@ proc Init*(ctx: ptr Ctx, name: const_char_pp, ver, apiver: cint):cint {. redis_e
 
      let getapifuncptr:pointer = cast[ptr UncheckedArray[pointer]](ctx)[0]
      GetApi = cast[proc(name: const_char_pp, data: pointer): cint {.cdecl.}](cast[culong](getapifuncptr))
-#[
-     GetRedisApi("Alloc",Alloc)
-     GetRedisApi("Realloc",Realloc)
-     GetRedisApi("CreateCommand",CreateCommand)
-     GetRedisApi("ReplyWithLongLong",ReplyWithLongLong)
-     GetRedisApi("SetModuleAttribs",SetModuleAttribs)
-     GetRedisApi("ReplyWithSimpleString", ReplyWithSimpleString)
-     SetModuleAttribs(ctx,name,ver,apiver)
-]#
 
      GetRedisApi(Alloc)
      GetRedisApi(Realloc)
      GetRedisApi(CreateCommand)
+
      GetRedisApi(ReplyWithLongLong)
-     GetRedisApi(SetModuleAttribs)
      GetRedisApi(ReplyWithSimpleString)
+     GetRedisApi(ReplyWithArray)
+     GetRedisApi(ReplySetArrayLength)
+
+     GetRedisApi(SetModuleAttribs)
      SetModuleAttribs(ctx,name,ver,apiver)
 
-
      result = 0
+
+proc arrayOf*(ctx: ptr Ctx, data:seq, datatype: string):cint {. inline .} = 
+    discard ReplyWithArray(ctx,data.len)
+    for item in data: 
+        case datatype:
+            of "ll":
+                discard ReplyWithLongLong(ctx,cast[clonglong](item))
+            of "s":
+                discard ReplyWithSimpleString(ctx,$item)
+            else: 
+                result = 1
+    
+        
